05cb61dd8e74de9824124f7f88f0629a3cff9eb3
Danishkar <danishkar.s@gmail.com>
2023-03-31 01:53:42 +0530

    changes

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ app.post("/",function(req,res){
     res.redirect("/");
 })
 
-app.listen(3000,function(){
+app.listen(8000 ,function(){
     console.log("Server is up and running on port 3000")
 });
 
